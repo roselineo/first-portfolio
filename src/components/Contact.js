1f76a3be4ac93b4c016168e2cc507f572a552976
roselineo <anyaeler@gmail.com>
2024-03-07 12:27:40 +0100

    finished the mobile version

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import contactImage from "../images/oil.jpg"
 
 function Contact() {
     return (
-        <section className="contact">
+        <section className="contact" id="contact">
             <h1>Contact Me</h1>
             <div className="contact-details">
                 <div className="contact-image">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,39 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
+import React, { useState } from "react"
 import { FaBars, FaTimes } from "react-icons/fa"
 import "./Header.css"
+import cv from "../cv/doc.docx"
 
 function Header() {
+    const [barTimes, setBarTimes] = useState(true)
+    
+    const handleClick = () => {
+        const menu = document.querySelector(".menu")
+        menu.classList.toggle("show")
+        setBarTimes(!barTimes)
+    }
     return (
         <div className="header">
             <div className="logo-div">ROSELINE OBINNA</div>
-            <div className="mobile-menu">
-                <FaBars />
+            <div className="mobile-menu" onClick={handleClick}>
+                {barTimes ? <FaBars /> : <FaTimes />}
             </div>
             <div className="menu">
                 <ul>
                     <li>
-                        <Link to="#about" className="menu-item">About</Link>
+                        <a href="#about" className="menu-item" onClick={handleClick}>About</a>
                     </li>
                     <li>
-                        <Link to="#skills" className="menu-item">Skills</Link>
+                        <a href="#skills" className="menu-item" onClick={handleClick}>Skills</a>
                     </li>
                     <li>
-                        <Link to="#projects" className="menu-item">Projects</Link>
+                        <a href="#projects" className="menu-item" onClick={handleClick}>Projects</a>
                     </li>
                     <li>
-                        <Link to="#contact" className="menu-item">Contact</Link>
+                        <a href="#contact" className="menu-item" onClick={handleClick}>Contact</a>
                     </li>
                     <li>
-                        <Link to="#" className="menu-item">Download My CV</Link>
+                        <a href={cv} className="menu-item" onClick={handleClick}>Download My CV</a>
                     </li>
                 </ul>
             </div>
